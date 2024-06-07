bc2c596ac0b54105ef26ec0ab4133e336f2f01c3
[yousincha] <[yousincha@naver.com]>
2024-06-07 17:01:07 +0900

    20240607-2 (joinform handlesubmit에러 수정) commit

--- pages/joinform.js
+++ pages/joinform.js
@@ -10,26 +10,26 @@ import {
   Select,
   MenuItem,
 } from "@mui/material";
+import { styled } from "@mui/system";
 import axios from "axios";
 import { useRouter } from "next/router";
-import styled from "@emotion/styled";
 
-const StyledContainer = styled(Container)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  max-width: 500px;
-  height: 100%;
-  margin: 0 auto;
-`;
+const StyledContainer = styled(Container)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  maxWidth: "500px",
+  height: "100%",
+  margin: "0 auto",
+});
 
-const StyledForm = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-`;
+const StyledForm = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+});
 
 const JoinForm = () => {
   const router = useRouter();
@@ -74,65 +74,67 @@ const JoinForm = () => {
       <Typography variant="h4" component="h1" gutterBottom>
         회원가입
       </Typography>
-      <StyledForm onSubmit={handleSubmit}>
-        <TextField
-          label="회원이름"
-          type="text"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <TextField
-          label="이메일"
-          type="email"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          label="암호"
-          type="password"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          required
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <TextField
-          label="생년월일"
-          type="date"
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          InputLabelProps={{ shrink: true }}
-          required
-          value={birthDate}
-          onChange={(e) => setBirthDate(e.target.value)}
-        />
-        <FormControl fullWidth variant="outlined" margin="normal" required>
-          <InputLabel>성별</InputLabel>
-          <Select value={gender} onChange={handleChange} label="성별">
-            <MenuItem value="M">남성</MenuItem>
-            <MenuItem value="F">여성</MenuItem>
-          </Select>
-        </FormControl>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          size="large"
-          fullWidth
-        >
-          회원가입
-        </Button>
-      </StyledForm>
+      <form onSubmit={handleSubmit}>
+        <StyledForm>
+          <TextField
+            label="회원이름"
+            type="text"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            label="이메일"
+            type="email"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="암호"
+            type="password"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            label="생년월일"
+            type="date"
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            required
+            value={birthDate}
+            onChange={(e) => setBirthDate(e.target.value)}
+          />
+          <FormControl fullWidth variant="outlined" margin="normal" required>
+            <InputLabel>성별</InputLabel>
+            <Select value={gender} onChange={handleChange} label="성별">
+              <MenuItem value="M">남성</MenuItem>
+              <MenuItem value="F">여성</MenuItem>
+            </Select>
+          </FormControl>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+          >
+            회원가입
+          </Button>
+        </StyledForm>
+      </form>
     </StyledContainer>
   );
 };
