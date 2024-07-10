ed9d49d15e2fecd33fdfcea061d2332d4826bc59
[yousincha] <[yousincha@naver.com]>
2024-07-10 15:38:31 +0900

    20240710-2 (index 이미지 효과 변경) commit

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Container, Typography } from "@mui/material";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 import styles from "./styles/HomePage.module.css";
 
 const HomePage = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
+  const images = [`/main1.jpg`, `/main2.jpg`, `/main3.jpg`];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const slideInterval = useRef(null);
+
+  useEffect(() => {
+    startSlideShow();
+    return () => stopSlideShow();
+  }, []);
+
+  const startSlideShow = () => {
+    slideInterval.current = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
   };
 
-  const images = [
-    "https://cdn.pixabay.com/photo/2017/08/05/12/19/dress-2583113_1280.jpg",
-    "https://cdn.pixabay.com/photo/2021/10/03/14/29/scarves-6678139_1280.jpg",
-    "https://cdn.pixabay.com/photo/2017/02/08/02/56/booties-2047596_1280.jpg",
-  ];
+  const stopSlideShow = () => {
+    clearInterval(slideInterval.current);
+  };
+
+  const goToSlide = (index) => {
+    setCurrentIndex(index);
+    stopSlideShow();
+    startSlideShow();
+  };
 
   return (
-    <div>
+    <div className={styles.main}>
       <Container className={styles.container}>
         <Typography
           variant="h4"
@@ -35,17 +40,33 @@ const HomePage = () => {
         >
           CozyCorner
         </Typography>
-        <Slider {...settings}>
-          {images.map((image, index) => (
-            <div key={index} className={styles.imageContainer}>
-              <img
-                src={image}
-                alt={`쇼핑몰 이미지 ${index}`}
-                className={styles.bannerImage}
-              />
-            </div>
+        <div className={styles.slider}>
+          <div
+            className={styles.slides}
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {images.map((image, index) => (
+              <div key={index} className={styles.slide}>
+                <img
+                  src={image}
+                  alt={`쇼핑몰 이미지 ${index}`}
+                  className={styles.bannerImage}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.dots}>
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.dot} ${
+                currentIndex === index ? styles.active : ""
+              }`}
+              onClick={() => goToSlide(index)}
+            ></span>
           ))}
-        </Slider>
+        </div>
       </Container>
     </div>
   );
